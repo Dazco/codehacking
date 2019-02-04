cb25a3b176e73a59c6f0169d32071cc821a10cc9
Dazco <dazco777@gmail.com>
2019-02-04 23:24:14 +0100

    Users create page working without photos

---
+++ resources/views/admin/users/create.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Create Users</h1>
+    @include('includes.form_error')
+    {!! Form::open(['method'=>'POST','action'=>'AdminUsersController@store','files'=>true]) !!}
+        <div class="form-group">
+            {!! Form::label('name','Name:') !!}
+            {!! Form::text('name',null,['class'=>'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('email','Email:') !!}
+            {!! Form::email('email',null,['class'=>'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('role_id','Role:') !!}
+            {!! Form::select('role_id',$roles + ['create'=>'create new role'],null,['class'=>'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('is_active','Status') !!}
+            {!! Form::select('is_active',['1'=>'Active','0'=>'Not Active'],'0',['class'=>'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('password','Password:') !!}
+            {!! Form::password('password',['class'=>'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('password_confirmation','Confirm Password:') !!}
+            {!! Form::password('password_confirmation',['class'=>'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('profile','Profile Picture (optional):') !!}
+            {!! Form::file('profile',['class'=>'form-control-file']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::submit('Create User',['class'=>'btn btn-primary']) !!}
+        </div>
+    {!! Form::close() !!}
+@endsection
